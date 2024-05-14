cc9c6cf022ebd65d96429cfcd2828aa8b8deec42
Polina Rybachuk <polia6v@gmail.com>
2024-05-14 19:39:19 +0200

    Upd-14-05

--- src/ListWindow.java
+++ src/ListWindow.java
@@ -5,4 +5,24 @@ public class ListWindow {
     private JTabbedPane tabbedPane1;
     private JPanel membersPanel;
     private JPanel tasksPanel;
+    private JTable workersTable;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("ListWindow");
+        frame.setContentPane(new ListWindow().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+        Task task1 = new Task("Napisać klasę pracownik", Task.TaskStatus.IN_PROGRESS);
+        Task task2 = new Task("Dodać wielu pracowników", Task.TaskStatus.TODO);
+
+        Employee employee1 = new Employee("James", "Adams", "Programmer");
+
+    }
+
+    private void createUIComponents() {
+        workersTable = new JTable();
+        workersTable.setModel(new FinalTableModel("dupa"));
+    }
 }
